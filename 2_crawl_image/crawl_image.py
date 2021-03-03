22de576fa79d3218e76b12e1be12ec594ab7a8d7
sunxiang <824390715@qq.com>
2021-03-03 17:24:55 +0800

    modify

--- 2_crawl_image/crawl_image.py
+++ 2_crawl_image/crawl_image.py
@@ -1,18 +1,25 @@
 #男生必学的第一步
 #爬图片
 
+#注意点
+#1.网址的编码规则，根据不同编码做动态修改
+#   鼠标右击页面，查看源码 可以知道当前编码
+#2.图片匹配规则
+#   看清楚需要爬虫的图片的格式是https还是http
+#   看清需要爬虫的图片的后缀是jpg还是png
+
 import re
 import os
 import urllib
 import urllib.request
 
-def get_url(url):
+def get_url(url,decode):
     page =urllib.request.urlopen(url)
     html =page.read()
-    return html.decode("utf-8")
+    return html.decode(decode)
 
 def get_img(html):
-    reg = r'https://[^\s]*?\.jpg'
+    reg = r'http://[^\s]*?\.jpg'
     # 转换成一个正则对象
     imgre = re.compile(reg)
     # 表示在整个网页过滤出所有图片的地址，放在imgList中
@@ -33,5 +40,5 @@ def get_img(html):
         x = x + 1
     return imglist
 
-htmls =get_url("http://www.netbian.com")
+htmls =get_url("https://tieba.baidu.com/p/7222864124","utf-8")
 print(get_img(htmls))
