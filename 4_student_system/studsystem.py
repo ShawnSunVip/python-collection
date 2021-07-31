1c9f972c0a87c3e4ac547cba726dcb0d28508b37
sunxiang <824390715@qq.com>
2021-07-31 10:23:28 +0800

    over

--- 4_student_system/studsystem.py
+++ 4_student_system/studsystem.py
@@ -23,6 +23,18 @@ def add():
         id=input('entry student id:')
         if not id:
             break
+        flag = False
+        if os.path.exists(filename):
+            with open(filename, 'r', encoding='utf-8') as file:
+                file_list = file.readlines()
+                for item in file_list:
+                    d = dict(eval(item))
+                    if d['id'] == id:
+                        flag = True
+        if flag:
+            print('this student id is exist,please entry agin!')
+            continue
+
         name = input('entry student name:')
         if not name:
             break
@@ -57,7 +69,53 @@ def save(lst):
     file.close()
 
 def search():
-    pass
+    while True:
+        id =''
+        name =''
+        if os.path.exists(filename):
+            type = input('if id entry 1,if name entry 2:')
+            if type =='1':
+                id = input('please entry student id:')
+            elif type =='2':
+                name = input('please entry student name:')
+            else:
+                print('info error,please entry agin!')
+                search()
+
+            with open(filename, 'r', encoding='utf-8') as file:
+                file_list = file.readlines()
+                if file_list:
+                    flag = True
+                    for item in file_list:
+                        d = dict(eval(item))
+                        if id !='':
+                            if d['id'] == id:
+                                flag = False
+                                print('id:{0},name:{1},english:{2},java:{3},python:{4} \t'.format(d['id'], d['name'],
+                                                                                                  d['english'],
+                                                                                                  d['java'],
+                                                                                                  d['python']))
+                        elif name !='':
+                            if d['name'] == name:
+                                flag = False
+                                print('id:{0},name:{1},english:{2},java:{3},python:{4} \t'.format(d['id'], d['name'],
+                                                                                                  d['english'],
+                                                                                                  d['java'],
+                                                                                                  d['python']))
+                    if flag:
+                        print('not found student')
+                else:
+                    print('no student')
+
+        else:
+            print('no student')
+
+        ans = input('is continue?y/n')
+        if ans == 'y' or ans == 'Y':
+            continue
+        else:
+            break
+
 def delete():
     while True:
         id =input('entry student Id:')
@@ -92,16 +150,104 @@ def delete():
             else:
                 break
 
-
 def modify():
-    pass
+    while True:
+        id = input('entry student Id:')
+        if id != '':
+            if os.path.exists(filename):
+                with open(filename, 'r', encoding='utf-8') as file:
+                    student_list = file.readlines()
+            else:
+                student_list = []
+        if student_list:
+            with open(filename, 'w', encoding='utf-8') as wfile:
+                d = {}
+                flag =False
+                for item in student_list:
+                    d = dict(eval(item))
+                    if d['id'] != id:
+                        wfile.write(str(d) + "\n")
+                    else:
+                        print(f'find this student={id},please modify!')
+                        while True:
+                            try:
+                                d['name'] = input('entry student name:')
+                                d['english'] = int(input('English score:'))
+                                d['java'] = int(input('Java score:'))
+                                d['python'] = int(input('Python score:'))
+                            except:
+                                print("info error")
+                            else:
+                                break
+                        wfile.write(str(d)+"\n")
+                        flag =True
+                if flag:
+                    print(f'id={id} is updated!')
+                else:
+                    print(f'not find this student={id}')
+        else:
+            print('no student')
+            break
+
+        show()
+
+        ans = input('is continue?y/n')
+        if ans == 'y' or ans == 'Y':
+            continue
+        else:
+            break
 def sort():
-    pass
+    show()
+    if os.path.exists(filename):
+        with open(filename, 'r', encoding='utf-8') as file:
+            file_list = file.readlines()
+        student_list=[]
+        for item in file_list:
+            d = dict(eval(item))
+            student_list.append(d)
+    else:
+        return
+
+    asc_or_desc = input('请选择排序方式 0：升序，1：降序:')
+    if asc_or_desc =='0':
+        asc_or_desc_bool =False
+    elif asc_or_desc =='1':
+        asc_or_desc_bool =True
+    else:
+        print('error,please agin')
+        sort()
+
+    model =input('请选择排序方式 0：总成绩，1：英语，2：python，3：java：')
+    if model =='0':
+        student_list.sort(key=lambda x: x['english']+x['python']+x['java'],reverse=asc_or_desc_bool)
+    elif model =='1':
+        student_list.sort(key=lambda x: x['english'],reverse=asc_or_desc_bool)
+    elif model =='2':
+        student_list.sort(key=lambda x: x['python'],reverse=asc_or_desc_bool)
+    elif model =='3':
+        student_list.sort(key=lambda x: x['java'],reverse=asc_or_desc_bool)
+    else:
+        print('error,please agin')
+        sort()
+    showStudent(student_list)
+
 def total():
     pass
-def show():
-    pass
 
+def show():
+    if os.path.exists(filename):
+        with open(filename, 'r', encoding='utf-8') as file:
+            file_list = file.readlines()
+            student_list =[]
+            for item in file_list:
+                d = dict(eval(item))
+                student_list.append(d)
+            showStudent(student_list)
+
+def showStudent(student_list):
+    for item in student_list:
+        print('id:{0},name:{1},english:{2},java:{3},python:{4} \t'.format(item['id'], item['name'], item['english'], item['java'],
+                                                                          item['python']))
 
 def main():
     while True:
